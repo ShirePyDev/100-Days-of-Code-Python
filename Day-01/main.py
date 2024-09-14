12884b43abae0f065d923a731cbd28f47699ff8c
ShirePyDev <devkhalid45@gmail.com>
2024-09-14 11:46:53 +0300

    Day-01: Completed learning and practiced challenges with handout

---
+++ Day-01/main.py
@@ -0,0 +1,50 @@
+# Write your code below this line 👇
+# print("Hello world!")
+
+#       DIFFERENT TYPES OF VARIABLE👇
+#INTEGER
+#STRING
+# BOOLEAN
+# FLOAT
+#          STRING
+# name = input("Write your name here: ")
+# print(name)
+
+#           INTEGER
+# x = 10 + 20
+# print(x)
+#           FLOATING
+# y = float(9 + 10)
+# print(y)
+    #       BOOLEAN
+# T = True
+# F = False
+# print(T)
+
+# Using new line with your code👇
+# print ("Mix 500g of flour, 10g Yeast and 300ml Water in a bowl."
+#        "\nBake at 200 C for 30 minutes.")
+
+#   COUNTING A VARIABLE USING A len() Function👇.
+# Username = ("Please work hard and try to be your best version of your life!")
+# length = len(Username)
+# print(length)
+
+#       ASKING THE USER TO PUT HIS/HER NAME THEN COUNTING USING "len()"👇. 
+# Username = input("Enter your name: ")
+# Length =  len(Username)
+# print(Length)
+
+#   SWAPPING A VARIABLE TO ANOTHER VARIABLE👇.
+# glass1 = "milk"
+# glass2 = "juice"
+# temp = glass1
+# glass1 = glass2
+# glass2 = temp
+# print(glass2, glass1)
+
+# ANOTHER WAY WE CAN DO A SWAPPING, AND IT'S MORE EASY THAN THE OTHER ONE👇.
+# glass1 = "milk"
+# glass2 = "juice"
+# glass1, glass2 = glass2, glass1
+# print(glass1)
