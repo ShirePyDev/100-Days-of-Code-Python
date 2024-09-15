c3f2d11b8796458aaba0549156bffe280aca88fd
ShirePyDev <devkhalid45@gmail.com>
2024-09-15 16:20:26 +0300

    Learned Basic Operators, Type checking, Indexing, and F-strings in Python for Day-02

---
+++ Day-02/main.py
@@ -0,0 +1,27 @@
+        # Knowing the type of the Datatype using type().
+# print(type(False))
+# print(type("1234"))
+# print(type(1234))
+# print(type(123.21))
+
+        #   Counting the letters of user's name
+# print("Number of letters in your name: " + str(len(input("Enter your name\n"))))
+
+                # Using F string with multiple datatypes.
+# height = 1.63
+# name = "Khalid Shire"
+# winning = False
+# print(f'Your name is = {name}\n'
+#       f'Your height is = {height}\n'
+#       f'Your winning is = {winning}')
+
+            # Basic Operators and how they work
+            
+# print(3 * (3 + 3) / 3 - 3)
+# print(10 ** 10)
+# print(20 / 2)
+# print(20 + 90)
+# print(20 // 10)
+
+a = int("5") / int(2.7)
+print(a)
