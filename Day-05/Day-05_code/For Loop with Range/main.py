a895ba619fb537ce45976fb9170681345deba55a
ShirePyDev <devkhalid45@gmail.com>
2024-09-18 18:23:28 +0300

    Implemented and learned: for loop, range() function, Nested for loops and working for loop with If condition

---
+++ Day-05/Day-05_code/For Loop with Range/main.py
@@ -0,0 +1,23 @@
+        # range() function
+# for x in range(7):
+#     print(x)    
+
+        # range() with a two parametre
+# for x in range(2, 6):
+#     print(x) 
+
+        # range() function with three parametre.
+# for x in range(2, 20, 3):
+#     print(x)
+
+        # range() funcion with else Statement.
+# for x in range(6):
+#     print(x)
+# else:
+#     print("Finally finished!!")
+
+for x in range(6):
+    if x == 3:break     
+    print(x)
+else:
+    print("Finally finished!!")
---
+++ Day-05/Day-05_code/For Loop/main.py
@@ -0,0 +1,36 @@
+fruits = ["Banana", "Peach", "Pear", "Orange"]
+for fruit in fruits:
+    print(fruit)
+    print(fruit + "Khalid")
+    print(fruits)
+    
+    # Looping through a string
+for x in "banana":
+    print(x.)
+
+        # Exit the loop when the x is "Pear", And the break comes before the print. 
+fruits = ["Banana", "Peach", "Pear", "Orange"]
+for x in fruits:
+    if x == "Pear":
+        break
+    print(x)
+
+
+        # Continue statement, we can stop the current iteration of the loop, and cuntinue with the next.
+fruits = ["Banana", "Peach", "Pear", "Orange"]
+for x in fruits:
+    if x == "Pear":
+        continue
+    print(x)
+
+    #  Nested loops
+adj = ["red", "big", "small"]
+fruits = ["apple", "banana", "mango"]
+for x in adj:
+    for y in fruits:
+        print(x, y)
+
+
+        
+
+        
