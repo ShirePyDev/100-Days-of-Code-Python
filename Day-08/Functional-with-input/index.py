2639e45400028099bba824beea4d017e189ffe0a
ShirePyDev <devkhalid45@gmail.com>
2024-10-05 17:57:56 +0300

    Learnded and implemented Funtional Input and next will start Ceaser Game

--- Day-08/Functional-with-input/index.py
+++ Day-08/Functional-with-input/index.py
@@ -1,2 +1,19 @@
-# Here is DAY-08
-print("Hello world")
+import random
+
+word_list = ["aardvark", "baboon", "camel"]
+
+# TODO-1 - Randomly choose a word from the word_list and assign it to a variable called chosen_word. Then print it.
+chosen_random = random.choice(word_list)
+print(chosen_random)
+
+# TODO-2 - Ask the user to guess a letter and assign their answer to a variable called guess. Make guess lowercase.
+guess = input("Guess a letter: ").lower()
+
+# TODO-3 - Check if the letter the user guessed (guess) is one of the letters in the chosen_word. Print "Right" if it
+#  is, "Wrong" if it's not.
+
+for x in chosen_random:
+    if x == guess:
+        print("Right")
+    else:
+        print("Wrong")
