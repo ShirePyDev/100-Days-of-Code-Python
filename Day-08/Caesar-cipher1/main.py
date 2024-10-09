5bda22515992fdaa0d2c2101cbf535dc1c539f90
ShirePyDev <devkhalid45@gmail.com>
2024-10-10 02:09:55 +0300

    Implement Caesar cipher for encryption and decryption with input handling

---
+++ Day-08/Caesar-cipher1/main.py
@@ -0,0 +1,19 @@
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n").lower()
+text = input("Type your message:\n").lower()
+shift = int(input("Type the shift number:\n"))
+# TODO-1: Create a function called 'encrypt()' that takes 'original_text' and 'shift_amount' as 2 inputs.
+
+# TODO-2: Inside the 'encrypt()' function, shift each letter of the 'original_text' forwards in the alphabet
+#  by the shift amount and print the encrypted text.
+# TODO-4: What happens if you try to shift z forwards by 9? Can you fix the code?
+
+def encrypt(original_text, shift_amount):
+    Ceaser = ""
+    for letter in original_text:
+        shift_Position = (alphabet.index(letter) + shift_amount) % 26
+        # shift_Position = shift_Position.len([alphabet]) % 26
+        Ceaser += alphabet[shift_Position]
+    print(Ceaser)
+encrypt(original_text= text, shift_amount= shift)
--- Day-08/Functional-with-input/index.py
+++ Day-08/Functional-with-input/index.py
@@ -1,19 +1,5 @@
-import random
-
-word_list = ["aardvark", "baboon", "camel"]
-
-# TODO-1 - Randomly choose a word from the word_list and assign it to a variable called chosen_word. Then print it.
-chosen_random = random.choice(word_list)
-print(chosen_random)
-
-# TODO-2 - Ask the user to guess a letter and assign their answer to a variable called guess. Make guess lowercase.
-guess = input("Guess a letter: ").lower()
-
-# TODO-3 - Check if the letter the user guessed (guess) is one of the letters in the chosen_word. Print "Right" if it
-#  is, "Wrong" if it's not.
-
-for x in chosen_random:
-    if x == guess:
-        print("Right")
-    else:
-        print("Wrong")
+def my_function(name, location):
+    print(f"My name is {name}")
+    print(f"My name is {location}")
+    
+my_function("Khalid", "Seoul")
